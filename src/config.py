2c87a24fdf1df902afdc5f52c1926a62965a1f4f
itsrealfake <107151866+itsrealfake@users.noreply.github.com>
2025-05-09 12:38:59 -0500

    revert changes for local development

--- src/config.py
+++ src/config.py
@@ -1,6 +1,12 @@
 # config.py
-NEO4J_URI = "bolt://localhost:7687"  # Bolt protocol (matches Docker port mapping)
+NEO4J_URI = "bolt://neoj4:7687"  # Bolt protocol (matches Docker port mapping)
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "password"  # Matches NEO4J_AUTH in docker-compose.yml
 REPO_URL = "https://github.com/bitcoin/bitcoin.git"  # Bitcoin Repo
-LOCAL_REPO_PATH = "data/user_supplied_repo"  # Where the repo will be cloned
+LOCAL_REPO_PATH = "downloaded_repo"  # Where the repo will be cloned
+
+
+
+## Uncomment for @itsrealfake local development
+# LOCAL_REPO_PATH = "data/user_supplied_repo"  # Where the repo will be cloned
+# NEO4J_URI = "bolt://localhost:7687"  # Bolt protocol (matches Docker port mapping)
