d194dc242f130727f20ea9c2a5aee6f7312e413e
itsrealfake <107151866+itsrealfake@users.noreply.github.com>
2025-05-09 12:39:41 -0500

    typo

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 # config.py
-NEO4J_URI = "bolt://neoj4:7687"  # Bolt protocol (matches Docker port mapping)
+NEO4J_URI = "bolt://neoj4j:7687"  # Bolt protocol (matches Docker port mapping)
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "password"  # Matches NEO4J_AUTH in docker-compose.yml
 REPO_URL = "https://github.com/bitcoin/bitcoin.git"  # Bitcoin Repo
